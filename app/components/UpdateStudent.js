09aff00b6cecbd0024e5c3c58951bf87c1642847
Nayana Limaye <nalimaye@yahoo.com>
2019-12-19 11:20:19 -0500

    Checking for duplicate names and emails for Updating A Student

--- app/components/UpdateStudent.js
+++ app/components/UpdateStudent.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { thunkToUpdateAStudentCreator } from '../reducers/studentsReducer';
+import {
+  thunkToUpdateAStudentCreator,
+  thunkToGetStudentsCreator,
+} from '../reducers/studentsReducer';
 import StudentForm from './StudentForm';
 
 const mapStateToProps = state => {
-  return { errorMessage: state.errorsReducer.errorMessage };
+  return {
+    errorMessage: state.errorsReducer.errorMessage,
+    students: state.studentsReducer.students,
+  };
 };
 const mapDispatchToProps = dispatch => {
   return {
     thunkToUpdateAStudentCreator: studentToUpdate =>
       dispatch(thunkToUpdateAStudentCreator(studentToUpdate)),
+    thunkToGetStudentsCreator: () => dispatch(thunkToGetStudentsCreator()),
   };
 };
 
@@ -23,12 +30,16 @@ class UpdateStudent extends React.Component {
       email: '',
       imageUrl: '',
       gpa: '0.0',
+      errorMsg: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkForDuplicateName = this.checkForDuplicateName.bind(this);
+    this.checkForDuplicateEmail = this.checkForDuplicateEmail.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    await this.props.thunkToGetStudentsCreator();
     this.setState({
       id: this.props.student.id,
       firstName: this.props.student.firstName,
@@ -45,16 +56,53 @@ class UpdateStudent extends React.Component {
     });
   }
 
-  handleSubmit() {
-    this.props.thunkToUpdateAStudentCreator(this.state);
+  checkForDuplicateName(updatedStudent) {
+    const newFullName =
+      updatedStudent.firstName + ' ' + updatedStudent.lastName;
+    const duplicate = this.props.students.filter(
+      student =>
+        student.fullName === newFullName && student.id !== updatedStudent.id
+    );
+    if (duplicate.length > 0) return true;
+    else return false;
+  }
+
+  checkForDuplicateEmail(updatedStudent) {
+    const newEmail = updatedStudent.email;
+    const duplicate = this.props.students.filter(
+      student => student.email === newEmail && student.id !== updatedStudent.id
+    );
+    if (duplicate.length > 0) return true;
+    else return false;
+  }
+
+  async handleSubmit(event) {
+    const updatedStudent = {
+      id: this.state.id,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      imageUrl: this.state.imageUrl,
+      gpa: this.state.gpa,
+    };
+    if (this.checkForDuplicateName(updatedStudent) === true) {
+      event.preventDefault();
+      this.setState({ errorMsg: 'Student with this name already exists.' });
+    } else if (this.checkForDuplicateEmail(updatedStudent) === true) {
+      event.preventDefault();
+      this.setState({ errorMsg: 'This email address is already used.' });
+    } else {
+      await this.props.thunkToUpdateAStudentCreator(updatedStudent);
+      console.log('updated');
+      this.setState({ errorMsg: this.props.errorMessage });
+    }
   }
 
   render() {
-    const { errorMessage } = this.props;
     return (
       <StudentForm
         {...this.state}
-        errorMessage={errorMessage}
+        errorMessage={this.state.errorMsg}
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
         buttonName="Update This Student"
