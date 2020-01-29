d7f5ad3daf783760d8d950b315c23f4ae15c97a8
David Patlut <dpatlut@gmail.com>
2020-01-29 13:34:47 -0500

    removing vollyball

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@
 
 const express = require('express');
 const path = require('path');
-const volleyball = require('volleyball');
 
 const app = express();
 
-// logging middleware
-app.use(volleyball);
+// // logging middleware
+// app.use(volleyball);
 
 // body parsing middleware
 app.use(express.json());
